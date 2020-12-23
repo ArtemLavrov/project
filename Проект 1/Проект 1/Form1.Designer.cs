df7102ff915ba3289112919166f3c90e4b16ee6e
Artem <artiomlavrov68@gmail.com>
2020-12-23 16:34:18 +0300

    ALL PROGECT

---
+++ Проект 1/Проект 1/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Проект_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SEARCH = new System.Windows.Forms.Button();
+            this.TEMPERATURE2 = new System.Windows.Forms.TextBox();
+            this.TEMPERATURE1 = new System.Windows.Forms.TextBox();
+            this.CITY = new System.Windows.Forms.TextBox();
+            this.WeakWeather = new System.Windows.Forms.Button();
+            this.labelCity = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SEARCH
+            // 
+            this.SEARCH.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.SEARCH.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SEARCH.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SEARCH.Location = new System.Drawing.Point(609, 12);
+            this.SEARCH.Name = "SEARCH";
+            this.SEARCH.Size = new System.Drawing.Size(98, 28);
+            this.SEARCH.TabIndex = 0;
+            this.SEARCH.Text = "ПОИСК";
+            this.SEARCH.UseVisualStyleBackColor = false;
+            this.SEARCH.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TEMPERATURE2
+            // 
+            this.TEMPERATURE2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.TEMPERATURE2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TEMPERATURE2.Font = new System.Drawing.Font("Century Gothic", 65.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TEMPERATURE2.Location = new System.Drawing.Point(431, 145);
+            this.TEMPERATURE2.Multiline = true;
+            this.TEMPERATURE2.Name = "TEMPERATURE2";
+            this.TEMPERATURE2.ReadOnly = true;
+            this.TEMPERATURE2.Size = new System.Drawing.Size(276, 114);
+            this.TEMPERATURE2.TabIndex = 1;
+            this.TEMPERATURE2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TEMPERATURE2.WordWrap = false;
+            // 
+            // TEMPERATURE1
+            // 
+            this.TEMPERATURE1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.TEMPERATURE1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TEMPERATURE1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.TEMPERATURE1.Font = new System.Drawing.Font("Century Gothic", 65.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TEMPERATURE1.Location = new System.Drawing.Point(109, 145);
+            this.TEMPERATURE1.Name = "TEMPERATURE1";
+            this.TEMPERATURE1.ReadOnly = true;
+            this.TEMPERATURE1.Size = new System.Drawing.Size(240, 107);
+            this.TEMPERATURE1.TabIndex = 1;
+            this.TEMPERATURE1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TEMPERATURE1.WordWrap = false;
+            // 
+            // CITY
+            // 
+            this.CITY.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CITY.Location = new System.Drawing.Point(461, 12);
+            this.CITY.Name = "CITY";
+            this.CITY.Size = new System.Drawing.Size(142, 27);
+            this.CITY.TabIndex = 3;
+            this.CITY.Text = "Ввeдите город";
+            this.CITY.Click += new System.EventHandler(this.CITY_Click);
+            this.CITY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CITY_KeyPress);
+            // 
+            // WeakWeather
+            // 
+            this.WeakWeather.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.WeakWeather.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WeakWeather.Location = new System.Drawing.Point(189, 372);
+            this.WeakWeather.Name = "WeakWeather";
+            this.WeakWeather.Size = new System.Drawing.Size(354, 27);
+            this.WeakWeather.TabIndex = 4;
+            this.WeakWeather.Text = "Погода на неделю";
+            this.WeakWeather.UseVisualStyleBackColor = true;
+            this.WeakWeather.Click += new System.EventHandler(this.WeakWeather_Click);
+            // 
+            // labelCity
+            // 
+            this.labelCity.AutoSize = true;
+            this.labelCity.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.labelCity.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCity.Location = new System.Drawing.Point(-4, 70);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(204, 41);
+            this.labelCity.TabIndex = 7;
+            this.labelCity.Text = "Ярославль";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(719, 411);
+            this.Controls.Add(this.labelCity);
+            this.Controls.Add(this.WeakWeather);
+            this.Controls.Add(this.CITY);
+            this.Controls.Add(this.TEMPERATURE1);
+            this.Controls.Add(this.TEMPERATURE2);
+            this.Controls.Add(this.SEARCH);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SEARCH;
+        private System.Windows.Forms.TextBox TEMPERATURE2;
+        private System.Windows.Forms.TextBox TEMPERATURE1;
+        private System.Windows.Forms.TextBox CITY;
+        private System.Windows.Forms.Button WeakWeather;
+        private System.Windows.Forms.Label labelCity;
+    }
+}
+
---
+++ Проект 1/Проект 1/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+
+
+
+namespace Проект_1
+{
+    public partial class Form1 : Form
+    {
+        public string urlY;
+        public string urlG;
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            var defUrlY = $"https://yandex.ru/pogoda/yaroslavl";
+            urlY = defUrlY;
+            var doc1 = new HtmlAgilityPack.HtmlDocument();
+            var web1 = new HtmlWeb();
+            doc1.LoadHtml(web1.Load(defUrlY).Text);
+            var span1 = doc1.DocumentNode.SelectNodes("//span[contains(@class, 'temp__value')]");
+            var temp1 = span1[1].InnerHtml;
+
+
+            var defUrlG = $"https://www.gismeteo.ru/weather-yaroslavl-4313/";
+            urlG = defUrlG;
+            var doc2 = new HtmlAgilityPack.HtmlDocument();
+            var web2 = new HtmlWeb();
+
+            doc2.LoadHtml(web2.Load(defUrlG).Text);
+            var span2 = doc2.DocumentNode.SelectNodes("//span[contains(@class, 'js_value tab-weather__value_l')]");
+            var temp2 = span2[0].InnerText.Split(new char[] { ';', ' ','\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (temp2[0] == "&minus")
+                TEMPERATURE2.Text = "−" + temp2[1] + (char)176;
+            else
+                TEMPERATURE2.Text = temp2[1] + (char)176;
+
+            TEMPERATURE1.Text = temp1 + (char)176;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            searchCityY(CITY.Text);
+            searchCityGis(CITY.Text);            
+
+        }
+        public void YandexTemp(string url)
+        {
+            var urll = url;           
+            var doc1 = new HtmlAgilityPack.HtmlDocument();
+            var web1 = new HtmlWeb();
+            doc1.LoadHtml(web1.Load(urll).Text);         
+            var span1 = doc1.DocumentNode.SelectNodes("//span[contains(@class, 'temp__value')]");
+            TEMPERATURE1.Text = span1[1].InnerHtml + (char)176;
+            
+            
+        }
+        public void searchCityY (string value)
+        {      
+            var city = value;            
+            urlY = $"https://yandex.ru/pogoda/search?request={city}";            
+            var doc1 = new HtmlAgilityPack.HtmlDocument();
+            var web1 = new HtmlWeb();
+            try
+            {
+                doc1.LoadHtml(web1.Load(urlY).Text);
+                var tegA = doc1.DocumentNode.SelectNodes("//a[contains(@class, 'link place-list__item-name i-bem')]");
+                var temp1 = tegA[0].OuterHtml;
+
+                Regex regex = new Regex(@"/\d+\?via=srp");
+                MatchCollection match = regex.Matches(temp1);
+                foreach (var ch in match)
+                {
+                    urlY = $"https://yandex.ru/pogoda" + ch.ToString();
+                }
+                labelCity.Text = tegA[0].InnerText.Split(',')[0];
+
+                YandexTemp(urlY);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("\n Yandex: Не могу найти город :( \n или \n Такого города не существует\n", e.Message);
+                searchCityY("Ярославль");
+            }
+        }
+        public void searchCityGis(string value)
+        {
+            var city = value;            
+            var url1 = $"https://www.gismeteo.ru/search/{city}/";
+            var doc1 = new HtmlAgilityPack.HtmlDocument();
+            var web1 = new HtmlWeb();
+            try
+            {
+                doc1.LoadHtml(web1.Load(url1).Text);
+                var span2 = doc1.DocumentNode.SelectNodes("//section[contains(@class, 'catalog_block')]");
+
+                if (span2.Count > 1)
+                    span2[0].RemoveAll();
+
+                span2 = span2[0].SelectNodes("//a[contains(@class, 'catalog_link link blue fontM')]");
+
+                Regex regex = new Regex(@"/weather-\w+[-\w+]*-\d+/");
+                MatchCollection match = regex.Matches(span2[0].OuterHtml);
+                foreach (var ch in match)
+                {
+                    url1 = $"https://www.gismeteo.ru" + ch.ToString();
+                }
+                GismeteoTemp(url1);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("\n GISMETEO: Не могу найти город :( \n или \n Такого города не существует\n", e.Message);
+                searchCityGis("Ярославль");
+            }
+            
+
+        }
+        public void GismeteoTemp(string urlGs )
+        {
+
+            var url1 = urlGs;
+            urlG = url1;
+            var doc1 = new HtmlAgilityPack.HtmlDocument();
+            var web1 = new HtmlWeb();
+            doc1.LoadHtml(web1.Load(url1).Text);
+            var span2 = doc1.DocumentNode.SelectNodes("//span[contains(@class, 'js_value tab-weather__value_l')]");
+            
+
+            var temp2 = span2[0].InnerText.Split(new char[] { ';', ' ','\n' }, StringSplitOptions.RemoveEmptyEntries);
+            if (temp2.Length>1 && temp2[0] == "&minus")
+                TEMPERATURE2.Text = "-" + temp2[1]+(char)176;
+            else
+                TEMPERATURE2.Text = temp2[0] + (char)176;
+        }
+
+        private void WeakWeather_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(this);
+            form2.Show();
+            form2.Visible = true;
+        }
+
+        private void CITY_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == 13 && CITY.Text!="")
+            {
+                button1_Click(new object(), new EventArgs());
+                return;
+            }
+            char l = e.KeyChar;
+            if ((l < 'А' || l > 'я') && l != '\b' && l != ' ')
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void CITY_Click(object sender, EventArgs e)
+        {
+            CITY.Clear();
+        }
+               
+    }
+}
